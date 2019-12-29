4e2029615d1c705378c40d2f60e7e722166bb659
berenyilajos <berenyil03@gmail.com>
2019-12-29 15:04:59 +0100

    Change the annotation of ResultRestController from @Controller to @RestController

--- memorygame-app/memorygame/src/main/java/hu/fourdsoft/memorygame/controller/rest/ResultRestController.java
+++ memorygame-app/memorygame/src/main/java/hu/fourdsoft/memorygame/controller/rest/ResultRestController.java
@@ -14,15 +14,15 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 @RequestMapping(value = "/game/result", method = RequestMethod.POST,
 		consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
