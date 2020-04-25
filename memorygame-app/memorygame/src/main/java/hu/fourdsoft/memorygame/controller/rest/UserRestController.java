6d0034b3e6e62f824d5652bc648058798f149ec6
Lajos <berenyil03@gmail.com>
2020-04-25 14:24:47 +0200

    At swaghger-ui.html only swowing methods from memorygame-api.yaml file, not from the springdoc genarated

--- memorygame-app/memorygame/src/main/java/hu/fourdsoft/memorygame/controller/rest/UserRestController.java
+++ memorygame-app/memorygame/src/main/java/hu/fourdsoft/memorygame/controller/rest/UserRestController.java
@@ -3,8 +3,10 @@ package hu.fourdsoft.memorygame.controller.rest;
 import hu.fourdsoft.memorygame.common.dto.UsersDTO;
 import hu.fourdsoft.memorygame.exception.MyApplicationException;
 import hu.fourdsoft.memorygame.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import java.util.Random;
 
 @RestController
 @RequestMapping("/game")
+@Slf4j
 public class UserRestController {
 	@Autowired
 	private UserService userService;
@@ -26,6 +29,15 @@ public class UserRestController {
 		UsersDTO users = new UsersDTO();
 		users.setUsers(userService.findAll());
 		return users;
+	}
+
+	@RequestMapping(value = "/api-docs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> swaggerUiPage() throws MyApplicationException {
+		log.debug("UserController.swaggerUiPage >>>");
+		String yaml = ResourceReader.readFileToString("api/memorygame-api.yaml");
+		String json = ResourceReader.convertYamlToJson(yaml);
+		log.debug("<<< UserController.swaggerUiPage");
+		return ResponseEntity.ok(json);
 	}
 
 }
