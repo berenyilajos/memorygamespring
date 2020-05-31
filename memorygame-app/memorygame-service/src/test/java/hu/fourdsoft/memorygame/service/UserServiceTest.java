19e18794f0cbe0e1a9c8c16a1c1e020de85672fc
berenyilajos <berenyil03@gmail.com>
2020-05-31 09:19:20 +0200

    Change runner class from MockitoJUnitRunner to SpringJUnit4ClassRunner

--- memorygame-app/memorygame-service/src/test/java/hu/fourdsoft/memorygame/service/UserServiceTest.java
+++ memorygame-app/memorygame-service/src/test/java/hu/fourdsoft/memorygame/service/UserServiceTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Optional;
 
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserServiceTest {
 
 	@InjectMocks
