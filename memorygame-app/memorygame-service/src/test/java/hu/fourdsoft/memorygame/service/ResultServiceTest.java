14a9fda3b7d9814089753b9c04de837a6cb24d4c
Berényi Lajos <berenyil03@gmail.com>
2022-09-10 08:45:43 +0200

    remove double semiclon in ResultServiceTest

--- memorygame-app/memorygame-service/src/test/java/hu/fourdsoft/memorygame/service/ResultServiceTest.java
+++ memorygame-app/memorygame-service/src/test/java/hu/fourdsoft/memorygame/service/ResultServiceTest.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.data.domain.Pageable;;
+import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
 import java.util.Date;
